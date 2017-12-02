22473369f4dfbd7c01c81ebf0788b2e3081d7cce
minakawa <minakawa.daiki.as@gmail.com>
2017-12-02 21:37:43 +0900

    トップ画面デザイン

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,9 @@
 class HomeController < ApplicationController
 
   def index
-
+    data1 = PlayData.find_by_user(1)
+    data2 = PlayData.find_by_user(2)
+    @new_record = PlayData.find_by_update_score(data1, data2)
   end
 
   def upload
--- app/models/play_data.rb
+++ app/models/play_data.rb
@@ -22,13 +22,39 @@ class PlayData
   end
 
   def self.find_by_update_score(data1, data2)
-    data1.each do |record|
-      record.each do |k, v|
-
-        # data2.each do |record|
-        #   p record[k]
-        # end
+    normal_update_record = {}
+    hyper_update_record = {}
+    another_update_record = {}
+    data1.each do |record1|
+      data2.each do |record2|
+       if record1['タイトル'] == record2['タイトル']
+         if record1['NORMAL EXスコア'].to_i < record2['NORMAL EXスコア'].to_i
+           normal_update_record[record1['タイトル']] = {
+               diff: record2['NORMAL EXスコア'].to_i - record1['NORMAL EXスコア'].to_i,
+               date: record2['最終プレー日時'],
+               score: record2['NORMAL EXスコア'],
+               level: record2['NORMAL DJ LEVEL'],
+           }
+         end
+         if record1['HYPER EXスコア'].to_i < record2['HYPER EXスコア'].to_i
+           hyper_update_record[record1['タイトル']] = {
+               diff: record2['HYPER EXスコア'].to_i - record1['HYPER EXスコア'].to_i,
+               date: record2['最終プレー日時'],
+               score: record2['HYPER EXスコア'],
+               level: record2['HYPER DJ LEVEL'],
+           }
+         end
+         if record1['ANOTHER EXスコア'].to_i < record2['ANOTHER EXスコア'].to_i
+           another_update_record[record1['タイトル']] = {
+               diff: record2['ANOTHER EXスコア'].to_i - record1['ANOTHER EXスコア'].to_i,
+               date: record2['最終プレー日時'],
+               score: record2['ANOTHER EXスコア'],
+               level: record2['ANOTHER DJ LEVEL'],
+           }
+         end
+       end
       end
     end
+    [normal_update_record, hyper_update_record, another_update_record]
   end
 end
