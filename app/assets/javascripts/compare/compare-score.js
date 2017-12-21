642348ff099445fe57f002e7195e0758fc20b890
minakawa <minakawa.daiki.as@gmail.com>
2017-12-22 00:31:17 +0900

    [FIX]サンプル

--- app/assets/javascripts/compare/compare-score.js
+++ app/assets/javascripts/compare/compare-score.js
@@ -20,4 +20,25 @@ window.addEventListener('turbolinks:load', function() {
       location.href = '/compare?csv1=' + csv1_b + '&csv2='+ e.target.value;
     });
   }
+
+  if(document.getElementsByName('sample_compare_csv1')[0]){
+    var csv1_sa = parser.searchParams.get("csv1");
+    var csv2_sa = parser.searchParams.get("csv2");
+    if(!csv1_sa){ csv1_sa = 0; }
+    if(!csv2_sa){ csv2_sa = 1; }
+    document.getElementsByName('sample_compare_csv1')[0].selectedIndex = csv1_sa;
+    document.getElementsByName('sample_compare_csv1')[0].addEventListener('change', function (e) {
+      location.href = '/sample/compare?csv1='+ e.target.value + '&csv2=' + csv2_sa;
+    });
+  }
+  if(document.getElementsByName('sample_compare_csv2')[0]){
+    var csv1_sb = parser.searchParams.get("csv1");
+    var csv2_sb = parser.searchParams.get("csv2");
+    if(!csv1_sb){ csv1_sb = 0; }
+    if(!csv2_sb){ csv2_sb = 1; }
+    document.getElementsByName('sample_compare_csv2')[0].selectedIndex = csv2_sb;
+    document.getElementsByName('sample_compare_csv2')[0].addEventListener('change', function (e) {
+      location.href = '/sample/compare?csv1=' + csv1_sb + '&csv2='+ e.target.value;
+    });
+  }
 });
--- app/assets/javascripts/score/redirect-score.js
+++ app/assets/javascripts/score/redirect-score.js
@@ -1,9 +1,15 @@
 window.addEventListener('turbolinks:load', function() {
+  var parser = new URL(location.href);
   if(document.getElementsByName('csv')[0]){
-    var parser = new URL(location.href);
     document.getElementsByName('csv')[0].selectedIndex = parser.searchParams.get("csv");
     document.getElementsByName('csv')[0].addEventListener('change', function (e) {
       location.href = '/recent?csv='+ e.target.value;
+    });
+  }
+  if(document.getElementsByName('sample_csv')[0]){
+    document.getElementsByName('sample_csv')[0].selectedIndex = parser.searchParams.get("csv");
+    document.getElementsByName('sample_csv')[0].addEventListener('change', function (e) {
+      location.href = '/sample?csv='+ e.target.value;
     });
   }
 });
